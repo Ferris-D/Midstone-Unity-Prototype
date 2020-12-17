648006efa30bf4070cf1ec84dd3cc38766f8839c
Ferris-D <58061592+Ferris-D@users.noreply.github.com>
2020-12-17 16:30:55 -0500

    upload

---
+++ GAME 208 Midstone Prototype/Assets/Scripts/EnemyHitScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHitScript : MonoBehaviour
+{
+	//Used to check if the target has been hit
+	public bool isHit = false;
+
+	// Enemy Health
+	public int enemyHealth;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		enemyHealth = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		//If the target is hit
+		if (isHit == true)
+		{
+			enemyHealth -= 10;
+
+			// If health is zero or below delete itself
+			if (enemyHealth <= 0)
+            {
+				gameObject.GetComponent
+				<EnemyController>().Die();
+				//Debug.Log("Enemy Destroyed");
+
+				//Destroy(gameObject);
+			}
+
+			//Debug.Log("Enemy Hit");
+			isHit = false;
+		}
+
+	}
+}
---
+++ GAME 208 Midstone Prototype/Assets/Scripts/PlayerHit.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHit : MonoBehaviour
+{
+    public int playerHealth;
+    public bool isHit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isHit)
+        {
+            playerHealth -= 10;
+            if(playerHealth <= 0)
+            {
+                Debug.Log("Player has Died");
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+            isHit = false;
+        }
+    }
+}
